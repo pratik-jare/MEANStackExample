69d81d6f069d8748396c7df5f9c5e93a3ae6ba83
pratik-jare <jarepratikpravin@gmail.com>
2020-06-18 20:13:35 +0530

    adding post to backend,

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -9,11 +9,11 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
+    this.http.get<{ message: string; posts: Post[] }>("http://localhost:3000/api/posts")
+      .subscribe(postData => {
         this.posts = postData.posts;
         this.postsUpdated.next([...this.posts]);
       });
@@ -25,7 +25,11 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http.post<{ message: string }>("http://localhost:3000/api/posts", post)
+      .subscribe(responseData => {
+        console.log(responseData.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
