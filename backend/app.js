a8fe9d214c927f12534e2e21f7cf001adec3cfb7
pratik-jare <jarepratikpravin@gmail.com>
2020-06-24 19:08:41 +0530

    deleting post and updating post list using mongodb

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const Post = require('./models/post');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://Pratik_jare:PR@tik148@cluster0-1guyh.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://Pratik_jare:PR@tik148@cluster0-1guyh.mongodb.net/Mean-Stack-Example?retryWrites=true&w=majority', { useNewUrlParser: true })
   .then(() => {
     console.log('connected to Database !');
   })
@@ -35,29 +35,34 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content,
   });
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
+  Post.find().then((document) => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: document
+    });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    res.status(200).json({ message: "Posts deleted successfully!" });
   });
 });
 
 module.exports = app;
+
+
+// mongo compass connection command
+// mongo "mongodb+srv://cluster0-1guyh.mongodb.net/Mean-Stack-Example" --username Pratik_jare
+
+// change db using command in that -> use <dbname>
+//  help 
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -28,6 +28,10 @@ export class PostListComponent implements OnInit, OnDestroy {
       });
   }
 
+  onDelete(postId) {
+    this.postsService.deletePost(postId);
+  }
+
   ngOnDestroy() {
     this.postsSub.unsubscribe();
   }
