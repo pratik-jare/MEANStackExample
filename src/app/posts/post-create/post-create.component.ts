0a9ebf31574b6c6fb6b62708d6bd8c511b53684c
pratik-jare <jarepratikpravin@gmail.com>
2020-07-01 21:12:11 +0530

    changes from template driven to reactive formfrom

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../post.model';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { NgxSpinnerService } from "ngx-spinner";
@@ -12,6 +12,8 @@ import { NgxSpinnerService } from "ngx-spinner";
 })
 export class PostCreateComponent implements OnInit {
 
+  form: FormGroup;
+
   enteredTitle = '';
   enteredContent = '';
 
@@ -22,15 +24,13 @@ export class PostCreateComponent implements OnInit {
   constructor(private postsService: PostsService, private route: ActivatedRoute, private router: Router, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+    this.createform();
     this.spinner.show();
     this.route.queryParams.subscribe((params) => {
       if (params.postId) {
         this.mode = 'edit';
         this.postId = params.postId;
-        this.postsService.getPost(this.postId).subscribe(postData => {
-          this.post = { id: postData._id, title: postData.title, content: postData.content };
-          this.spinner.hide();
-        });
+        this.getDetails();
       } else {
         this.mode = 'create';
         this.postId = null;
@@ -39,16 +39,39 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid) {
+  createform() {
+    this.form = new FormGroup({
+      'title': new FormControl(null, { validators: [Validators.required] }),
+      'content': new FormControl(null, { validators: [Validators.required] }),
+    })
+  }
+
+  getDetails() {
+    this.postsService.getPost(this.postId).subscribe(postData => {
+      this.post = { id: postData._id, title: postData.title, content: postData.content };
+      this.spinner.hide();
+      this.post = {
+        id: postData._id,
+        title: postData.title,
+        content: postData.content
+      }
+      this.form.setValue({
+        'title': this.post.title,
+        'content': this.post.content
+      });
+    });
+  }
+
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
     if (this.mode === 'create') {
-      this.postsService.addPost(form.value.title, form.value.content);
-      form.resetForm();
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
+      this.form.reset();
       this.router.navigateByUrl('/');
     } else {
-      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
       this.router.navigateByUrl('/');
     }
   }
