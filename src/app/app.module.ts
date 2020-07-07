fdf4233998082e4975057547db3b0b58f994f01a
pratik-jare <jarepratikpravin@gmail.com>
2020-07-07 15:53:35 +0530

    added pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
@@ -34,8 +35,9 @@ import { AppRoutingModule } from './app-routing.module';
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    MatPaginatorModule,
     HttpClientModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Subscription } from 'rxjs';
+import { NgxSpinnerService } from "ngx-spinner";
+import { PageEvent } from '@angular/material/paginator';
 
 import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
-import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: "app-post-list",
@@ -11,28 +12,43 @@ import { NgxSpinnerService } from "ngx-spinner";
   styleUrls: ["./post-list.component.css"]
 })
 export class PostListComponent implements OnInit, OnDestroy {
-  // posts = [
-  //   { title: "First Post", content: "This is the first post's content" },
-  //   { title: "Second Post", content: "This is the second post's content" },
-  //   { title: "Third Post", content: "This is the third post's content" }
-  // ];
+
   posts: Post[] = [];
   private postsSub: Subscription;
+
+  isLoading = false;
+  totalPosts = 0;
+  postsPerPage = 2;
+  currentPage = 1;
+  pageSizeOptions = [1, 2, 5, 10];
 
   constructor(public postsService: PostsService, private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
     this.spinner.show();
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
     this.postsSub = this.postsService.getPostUpdateListener()
-      .subscribe((posts: Post[]) => {
-        this.posts = posts;
+      .subscribe((postData: { posts: Post[], postCount: number }) => {
+        this.totalPosts = postData.postCount;
+        this.posts = postData.posts;
         this.spinner.hide();
       });
   }
 
+  onChangePage(pageData: PageEvent) {
+    this.spinner.show();
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    this.spinner.hide();
+  }
+
   onDelete(postId: string) {
-    this.postsService.deletePost(postId);
+    this.spinner.show();
+    this.postsService.deletePost(postId).subscribe(() => {
+      this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    });
+    this.spinner.hide();
   }
 
   ngOnDestroy() {
